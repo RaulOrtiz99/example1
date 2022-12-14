1d444426285b00bce71d6374d77943e34f05f248
RaulOrtiz99 <raulalbertoortiz290199@hotmail.com>
2022-12-14 01:18:16 -0400

    agregando un ultimo cambio

--- lib/screens/postform.dart
+++ lib/screens/postform.dart
@@ -7,17 +7,27 @@ class PostForm extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("New Post form"),
-
+        title: const Text("New Post form"),
       ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
-        children: [
+        children: const[
           Center(
-            child: Text("LAS VALIDACIONES DEL FORMULARIO NO ME DIO TIEMPO DE HACERLAS",style: TextStyle(fontWeight: FontWeight.bold,),),
+            child: Text(
+              "LAS VALIDACIONES DEL FORMULARIO NO ME DIO TIEMPO DE HACERLAS",
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+            ),
           ),
+          SizedBox(height: 10,),
           Center(
-            child: Text("Perdon  :(",style: TextStyle(fontWeight: FontWeight.bold,),),
+            child: Text(
+              "Perdon  :(",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: Colors.redAccent,
+                fontSize: 25,
+              ),
+            ),
           ),
         ],
       ),
